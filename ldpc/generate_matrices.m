d2663986186232d52e5a73d67f3b8641faaadef8
Daryl <daryl@siglabs.com>
2016-12-09 16:45:41 -0700

    Finished script to generate sparse matrix representations.

--- ldpc/generate_matrices.m
+++ ldpc/generate_matrices.m
@@ -17,7 +17,7 @@ D = gf(H(m-g+1:m, n-m+1:n-m+g));
 E = gf(H(m-g+1:m, n-m+g+1:n));
 
 % Precompute dense phi matrix
-phi = E * inv(T) * B + D;
+phi = E * (T \ B) + D;
 inv_phi = inv(phi);
 
 %% Store the results in matrix files
@@ -36,15 +36,98 @@ for row = 1:m-g
                 fprintf(fid, ', %d', col);
             end
         end
-        fprintf(fid, '\n');
+        if row ~= m-g
+            fprintf(fid, '\n');
+        end
     end
 end
 
 fclose(fid);
+
 % Store sparse E matrix to compute -E * (inv(T) * (A * s))
+fid = fopen('matrix_E.txt', 'w+');
+
+for row = 1:m-g
+    first = 1;
+    for col = 1:m-g
+        if E(row, col) ~= 0
+            if first == 1
+                fprintf(fid, '%d', col);
+                first = 0;
+            else
+                fprintf(fid, ', %d', col);
+            end
+        end
+        if row ~= m-g
+            fprintf(fid, '\n');
+        end
+    end
+end
+
+fclose(fid);
 
 % Store sparse C matrix to compute C * s
+fid = fopen('matrix_C.txt', 'w+');
+
+for row = 1:g
+    first = 1;
+    for col = 1:n-m
+        if C(row, col) ~= 0
+            if first == 1
+                fprintf(fid, '%d', col);
+                first = 0;
+            else
+                fprintf(fid, ', %d', col);
+            end
+        end
+        if row ~= g 
+            fprintf(fid, '\n');
+        end
+    end
+end
+
+fclose(fid);
 
 % Store dense inv(phi) matrix to Compute p1 = -inv(phi) * E * (inv(T) * (A * s))
+fid = fopen('matrix_inv_phi.txt', 'w+');
+
+for row = 1:g
+    first = 1;
+    for col = 1:g
+        if inv_phi(row, col) ~= 0
+            if first == 1
+                fprintf(fid, '%d', col);
+                first = 0;
+            else
+                fprintf(fid, ', %d', col);
+            end
+        end
+        if col ~= g 
+            fprintf(fid, '\n');
+        end
+    end
+end
+
+fclose(fid);
 
 % Store sparse B matrix to compute B * p1
+fid = fopen('matrix_B.txt', 'w+');
+
+for row = 1:m-g
+    first = 1;
+    for col = 1:g
+        if B(row, col) ~= 0
+            if first == 1
+                fprintf(fid, '%d', col);
+                first = 0;
+            else
+                fprintf(fid, ', %d', col);
+            end
+        end
+        if col ~= m-g
+            fprintf(fid, '\n');
+        end
+    end
+end
+
+fclose(fid);
