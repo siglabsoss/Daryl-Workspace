f114f4957f56fbe1e96addac7b90374541955910
Daryl <daryl@siglabs.com>
2017-01-05 16:00:43 -0700

    Fixed a bug in the dual diagonal backsubstitution. It isn ow validated.

--- ldpc/ldpc_test_vectors.m
+++ ldpc/ldpc_test_vectors.m
@@ -2,12 +2,14 @@
 
 % Generate the dual diagonal matrix
 X = eye(16*8, 16*8); 
-X(9:end, 1:end-8) = X(9:end, 1:end-8) + eye(15*8, 15*8);
+X(1+16:end, 1:end-16) = X(16+1:end, 1:end-16) + eye(16*(8-1), 16*(8-1));
+X = gf(X);
 
 % Input and output vectors.
 p = zeros(16*8, 1);
 p(1:3:end) = 1;
-q = mod(X * p, 2);
+p = gf(p);
+q = X * p;
 
-input_vector = (reshape(q, 16, 8).') * 2.^((0:15)')
-output_vector = (reshape(p, 16, 8).') * 2.^((0:15)')
+input_vector = reshape(double(q ~= 0), 16, 8).' * 2.^((0:15)')
+output_vector = reshape(double(p ~= 0), 16, 8).' * 2.^((0:15)')
