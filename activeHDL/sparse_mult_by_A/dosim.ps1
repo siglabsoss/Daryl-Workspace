44a01f5ce9de691d8d6b102364211163b5b27802
Daryl Wasden <dwasden@outlook.com>
2017-01-06 12:27:31 -0800

    Added files for the sparse matrix multiplications.

---
+++ activeHDL/sparse_mult_by_A/dosim.ps1
@@ -0,0 +1,24 @@
+function Compile-Verilog
+{
+	param( [string]$Filename)
+	vlog -work work -sv2k12 -dbg $Filename
+
+	if ($LastExitCode -ne 0) {
+		echo "                                                                "
+		echo "    ############################################################"
+		echo "      Compilation of " + $Filename + " failed! "
+		echo "    ############################################################"
+		echo "                                                                "
+		exit
+	}
+}
+
+# Create the Work Library
+vlib work
+
+# Compile the Verilog Files
+Compile-Verilog tb_my_module.sv
+Compile-Verilog my_module.sv
+
+# Execute the Simulation
+vsimsa -do dosim.do
