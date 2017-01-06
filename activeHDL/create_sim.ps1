6a24552484013e51c20bed097ac25579b041677b
Daryl Wasden <dwasden@outlook.com>
2017-01-06 12:18:12 -0800

    Added a script for auto-generation of modules from template.

---
+++ activeHDL/create_sim.ps1
@@ -0,0 +1,48 @@
+param ([string]$name = "template", [bool]$verbose = $true)
+
+if ($name -eq "template") {
+    write-output "Usage: .\create_sim.ps1 -name module_name"
+    exit
+}
+
+$dirname = "" + $pwd + "\" + $name
+if (Test-Path $dirname) {
+    write-output "Path already exists. Choose another name."
+    exit
+}
+
+if ($verbose) {
+    write-output "Creating directory: " + $dirname
+}
+New-Item $dirname -type directory
+
+if ($verbose) {
+    write-output "Copying files from template to $name..."
+}
+$srcdir = "" + $pwd + "\template\*"
+$destdir = $dirname
+write-output $srcdir
+write-output $destdir
+Copy-Item $srcdir $destdir
+
+# Alter files
+$destfile = $destdir + "\my_module.sv"
+(Get-Content $destfile) -replace '\[my_module\]', $name | Set-Content $destfile
+
+$destfile = $destdir + "\tb_my_module.sv"
+(Get-Content $destfile) -replace '\[my_module\]', $name | Set-Content $destfile
+
+$destfile = $destdir + "\dosim.do"
+(Get-Content $destfile) -replace '\[my_module\]', $name | Set-Content $destfile
+
+$destfile = $destdir + "\dosim.ps1"
+(Get-Content $destfile) -replace '\[my_module\]', $name | Set-Content $destfile
+
+# Rename files
+$srcfile = $destdir + "\my_module.sv"
+$destfile = $destdir + "\" + $name + ".sv"
+Move-Item -path $srcfile -destination $destfile
+
+$srcfile = $destdir + "\tb_my_module.sv"
+$destfile = $destdir + "\tb_" + $name + ".sv"
+Move-Item -path $srcfile -destination $destfile
--- activeHDL/template/copy_files_here.bat
+++
@@ -1,2 +0,0 @@
-xcopy /Y Z:\Daryl-Workspace\icarus\test\tb_dual_diagonal_backsub.sv .
-xcopy /Y Z:\Daryl-Workspace\icarus\hdl\dual_diagonal_backsub.sv .
