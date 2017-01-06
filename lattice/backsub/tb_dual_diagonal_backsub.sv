66a9b1559d2c548068ff195d8d02138d6c7111c9
Daryl Wasden <dwasden@outlook.com>
2017-01-06 09:33:55 -0800

    Refixed some bugs that should have been pushed already but must not have been.

--- lattice/backsub/tb_dual_diagonal_backsub.sv
+++ lattice/backsub/tb_dual_diagonal_backsub.sv
@@ -34,8 +34,13 @@ end
 
 // debug variable declarations
 logic [31:0] glbl_err_count = 0;
-logic [31:0] increment_value = 1;
+logic [31:0] test_number = 1;
 logic [31:0] run_count = 0;
+
+// Used by check process, declared here so it
+// can be included in the final tall for the
+// global error count.
+logic [31:0] local_err_count = 0;
 
 task reset_all;
     i_reset = 1'b1;
@@ -45,6 +50,28 @@ task reset_all;
     @(negedge i_clock) i_reset = 1'b0;
 endtask: reset_all
 
+logic [WIDTH-1:0] input_values [0:NUM_WORDS-1] = {
+    37449,
+    56173,
+    28086,
+    46811,
+    56173,
+    28086,
+    46811,
+    56173
+};
+
+logic [WIDTH-1:0] output_values [0:NUM_WORDS-1] = {
+    37449,
+    18724,
+     9362,
+    37449,
+    18724,
+     9362,
+    37449,
+    18724
+};
+
 initial begin: stimulus
     i_reset = 1'b1;
     #1000;
@@ -52,12 +79,12 @@ initial begin: stimulus
 
     // Test 1: No data in = no data out.
     $display("Test 1 Started!");
-    increment_value = 1;
+    test_number = 1;
     reset_all();
     #1000;
     @(negedge i_clock) begin
-        i_in_valid = 1'b1;
-        #(NUM_WORDS);
+        i_in_valid = 1'b0;
+        #(NUM_WORDS*10);
     end
     i_in_valid = 1'b0;
     #1000;
@@ -68,7 +95,76 @@ initial begin: stimulus
     #100;
     $display("Test 1 Done!");
 
+    // Test 2: All zeros in = all zeros out.
+    $display("Test 2 Started!");
+    test_number = 2;
+    reset_all();
+    #1000;
+    @(negedge i_clock) begin
+        i_in_data = 0;
+        i_in_valid = 1'b1;
+        #(NUM_WORDS*10);
+    end
+    i_in_valid = 1'b0;
+    #10000;
+    if (run_count != NUM_WORDS) begin
+        $display("Error: Test 2 failed! Expected %d outputs, but %d outputs received.", NUM_WORDS, run_count);
+        glbl_err_count++;
+    end
+    #100;
+    $display("Test 2 Done!");
+
+    // Test 3: All zeros in = all zeros out.
+    $display("Test 3 Started!");
+    test_number = 3;
+    reset_all();
+    #1000;
+    for (integer count = 0; count < NUM_WORDS; count = count + 1) begin
+        @(negedge i_clock) begin
+            i_in_data = input_values[count];
+            i_in_valid = 1'b1;
+        end
+        #10;
+    end
+    i_in_valid = 1'b0;
+    #10000;
+    if (run_count != NUM_WORDS) begin
+        $display("Error: Test 3 failed! Expected %d outputs, but %d outputs received.", NUM_WORDS, run_count);
+        glbl_err_count++;
+    end
+    #100;
+    $display("Test 3 Done!");
+
+    // Test 4: All zeros in = all zeros out.
+    $display("Test 4 Started!");
+    test_number = 4;
+    reset_all();
+    #1000;
+    for (integer count = 0; count < NUM_WORDS; count = count + 1) begin
+        @(negedge i_clock) begin
+            i_in_data = input_values[count];
+            i_in_valid = 1'b1;
+        end
+        #10;
+    end
+    for (integer count = 0; count < NUM_WORDS; count = count + 1) begin
+        @(negedge i_clock) begin
+            i_in_data = input_values[count];
+            i_in_valid = 1'b1;
+        end
+        #10;
+    end
+    i_in_valid = 1'b0;
+    #10000;
+    if (run_count != 2*NUM_WORDS) begin
+        $display("Error: Test 4 failed! Expected %d outputs, but %d outputs received.", 2*NUM_WORDS, run_count);
+        glbl_err_count++;
+    end
+    #100;
+    $display("Test 4 Done!");
+
     // Finished
+    glbl_err_count <= glbl_err_count + local_err_count;
     #10000;
     $display("Simulation done!");
     $finish();
@@ -76,24 +172,34 @@ initial begin: stimulus
 end
 
 // Tests the output sequence to make sure it matches the input
-logic [31:0] local_err_count = 0;
-logic [31:0] stored_value = 0;
 always @(posedge i_clock) begin: seq_check
     if (i_reset == 1'b1) begin
-        stored_value <= 0;
         run_count <= 0;
     end else begin
-        // Validate incrementing output sequence
-        if (o_out_valid == 1'b1) begin
-            if (o_out_data != stored_value) begin
-                $display("Error: Output of %d expected, but received %d.", stored_value, o_out_data);
+        // Validate all zero sequence
+        if ((o_out_valid == 1'b1) && (test_number == 2)) begin
+            if (o_out_data != 0) begin
+                $display("Error: Output of 0 expected, but received %d.", o_out_data);
                 local_err_count++;
             end
-            //$display("    %d", uut.out_data_reg);
-
-            // Increment value for next time
+        end
+        // Validate all zero sequence
+        if ((o_out_valid == 1'b1) && (test_number == 3)) begin
+            if (o_out_data != output_values[run_count]) begin
+                $display("Error: Output of %d expected, but received %d.", output_values[run_count], o_out_data);
+                local_err_count++;
+            end
+        end
+        // Validate all zero sequence
+        if ((o_out_valid == 1'b1) && (test_number == 3)) begin
+            if (o_out_data != output_values[run_count % NUM_WORDS]) begin
+                $display("Error: Output of %d expected, but received %d.", output_values[run_count], o_out_data);
+                local_err_count++;
+            end
+        end
+        // Track number of outputs received
+        if (o_out_valid == 1'b1) begin
             run_count <= run_count + 1;
-            stored_value <= stored_value + increment_value;
         end
     end
 end
