4c78cbcba1c7271c9ae08040014911d3a409a065
Daryl Wasden <dwasden@outlook.com>
2017-01-13 11:55:12 -0800

    Added test 3 that fills up the buffer and dumps it.

--- activeHDL/ebr_fifo/tb_ebr_fifo.sv
+++ activeHDL/ebr_fifo/tb_ebr_fifo.sv
@@ -74,7 +74,7 @@ initial begin: stimulus
 
     // Test 2: Simple data test, count in = count out.
     $display("Test 2 Started!");
-    test_number = 1;
+    test_number = 2;
     reset_all();
     #1000;
     for (count = 0; count < DEPTH; count = count + 1) begin
@@ -95,9 +95,34 @@ initial begin: stimulus
     #100;
     $display("Test 2 Done!");
 
+    // Test 3: Simple data test, count in = count out.
+    $display("Test 3 Started!");
+    test_number = 3;
+    reset_all();
+    #1000;
+    for (count = 0; count < DEPTH; count = count + 1) begin
+        @(negedge i_clock) begin
+            i_in_data = count;
+            i_in_valid = 1'b1;
+            i_out_ready = 1'b0;
+            #10;
+        end
+    end
+    i_in_valid = 1'b0;
+    i_out_ready = 1'b1;
+    #(DEPTH*10);
+    if (run_count != DEPTH) begin
+        $display("Error: Test 3 failed! Expected %d outputs, but received %d.",
+            DEPTH, run_count);
+        glbl_err_count++;
+    end
+    #100;
+    $display("Test 3 Done!");
+
     // Finished
     #10000;
     glbl_err_count = glbl_err_count + local_err_count;
+    #10;
     $display("Simulation done!");
     $finish();
 
@@ -109,8 +134,15 @@ always @(posedge i_clock) begin: seq_check
         run_count <= 0;
     end else begin
         // Track number of outputs received
-        if (o_out_valid == 1'b1) begin
+        if ((o_out_valid == 1'b1) && (i_out_ready == 1'b1)) begin
             run_count <= run_count + 1;
+
+            if ((test_number == 2) || (test_number == 3)) begin
+                if (o_out_data != run_count) begin
+                    $display("Data Error! Expected %d, but received %d.", run_count, o_out_data);
+                    local_err_count++;
+                end
+            end
         end
     end
 end
