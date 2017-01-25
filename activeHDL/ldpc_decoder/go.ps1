170b9b858614640d204a8f9112588ace341604e6
Daryl Wasden <dwasden@outlook.com>
2017-01-25 13:50:17 -0800

    Moved minsigner files to a dedicated subdirectory.

--- activeHDL/ldpc_decoder/go.ps1
+++ activeHDL/ldpc_decoder/go.ps1
@@ -1,6 +1,22 @@
 function Compile-Verilog
 {
 	param( [string]$Filename)
+	vlog -work work -dbg $Filename
+
+	if ($LastExitCode -ne 0) {
+		echo "                                                                "
+		echo "    ############################################################"
+		$msg = "      Compilation of " + $Filename + " failed! "
+		echo $msg
+		echo "    ############################################################"
+		echo "                                                                "
+		exit
+	}
+}
+
+function Compile-SystemVerilog
+{
+	param( [string]$Filename)
 	vlog -work work -sv2k12 -dbg $Filename
 
 	if ($LastExitCode -ne 0) {
@@ -14,12 +30,44 @@ function Compile-Verilog
 	}
 }
 
+function Compile-Verilog-Primitive
+{
+	param( [string]$Primitive)
+	$Filename = "C:\lscc\diamond\3.8_x64\cae_library\simulation\verilog\ecp5u"
+	$Filename += "\" + $Primitive + ".v"
+	Compile-Verilog $Filename
+}
+
 # Create the Work Library
 vlib work
 
 # Compile the Verilog Files
-Compile-Verilog tb_ldpc_decoder.sv
-Compile-Verilog ldpc_decoder.sv
+$sv_files = @(
+	"ldpc_adder_trees/ldpc_adder_trees.sv",
+	"adder_trees.sv",
+	"swapper.sv")
+
+$v_files = @(
+	"ldpc_adder_trees/adder_tree5.v")
+
+$primitives = @(
+	"GSR", "PUR", "ALU54B", "MULT18X18D",
+	"VHI", "VLO", "FD1P3IX", "UDFDL5SC_UDP_X")
+
+foreach ($sv_file in $sv_files)
+{
+	Compile-SystemVerilog $sv_file
+}
+
+foreach ($v_file in $v_files)
+{
+	Compile-Verilog $v_file
+}
+
+foreach ($primitive in $primitives)
+{
+	Compile-Verilog-Primitive $primitive
+}
 
 # Execute the Simulation
 vsimsa -do dosim.do
