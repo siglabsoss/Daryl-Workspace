4cf770fece04d0fa56cedbafd31828a02d436d9b
Daryl Wasden <dwasden@outlook.com>
2017-01-26 10:07:38 -0800

    Added tests for minsigner.

--- activeHDL/ldpc_decoder/ldpc_minsigner/ldpc_minsigner.sv
+++ activeHDL/ldpc_decoder/ldpc_minsigner/ldpc_minsigner.sv
@@ -14,208 +14,396 @@ module ldpc_minsigner (
     input  wire logic [7:0] i_data_a3,
     input  wire logic [7:0] i_data_a4,
     input  wire logic [7:0] i_data_a5,
-    input  wire logic       i_latch_inputs,
+    input  wire logic       i_valid,
     output      logic [7:0] o_data_a0,
     output      logic [7:0] o_data_a1,
     output      logic [7:0] o_data_a2,
     output      logic [7:0] o_data_a3,
     output      logic [7:0] o_data_a4,
     output      logic [7:0] o_data_a5,
+    output      logic       o_valid,
     input  wire logic       i_clock,
     input  wire logic       i_reset
 );
 
-logic [7:0] data_a0;
-logic       sign_a0;
-logic [7:0] data_a1;
-logic       sign_a1;
-logic [7:0] data_a2;
-logic       sign_a2;
-logic [7:0] data_a3;
-logic       sign_a3;
-logic [7:0] data_a4;
-logic       sign_a4;
-logic [7:0] data_a5;
-logic       sign_a5;
+logic [7:0] data_a0_reg0;
+logic       sign_a0_reg0;
+logic [7:0] data_a1_reg0;
+logic       sign_a1_reg0;
+logic [7:0] data_a2_reg0;
+logic       sign_a2_reg0;
+logic [7:0] data_a3_reg0;
+logic       sign_a3_reg0;
+logic [7:0] data_a4_reg0;
+logic       sign_a4_reg0;
+logic [7:0] data_a5_reg0;
+logic       sign_a5_reg0;
 
-logic [7:0] first_min;
-logic [7:0] second_min;
+logic [7:0] data_a0_reg1;
+logic       sign_a0_reg1;
+logic [7:0] data_a1_reg1;
+logic       sign_a1_reg1;
+logic [7:0] data_a2_reg1;
+logic       sign_a2_reg1;
+logic [7:0] data_a3_reg1;
+logic       sign_a3_reg1;
+logic [7:0] data_a4_reg1;
+logic       sign_a4_reg1;
+logic [7:0] data_a5_reg1;
+logic       sign_a5_reg1;
+
+logic [7:0] data_a0_reg2;
+logic       sign_a0_reg2;
+logic [7:0] data_a1_reg2;
+logic       sign_a1_reg2;
+logic [7:0] data_a2_reg2;
+logic       sign_a2_reg2;
+logic [7:0] data_a3_reg2;
+logic       sign_a3_reg2;
+logic [7:0] data_a4_reg2;
+logic       sign_a4_reg2;
+logic [7:0] data_a5_reg2;
+logic       sign_a5_reg2;
+
+logic [7:0] first_min_reg2;
 
 always_ff @ (posedge i_clock) begin
+    // Pipeline Stage 0
     // Extract magnitude of LLR (convert from signed to unsigned)
-    if (i_latch_inputs == 1'b1) begin
-        data_a0 <= i_data_a0[7] ? (-i_data_a0) : i_data_a0;
-        data_a1 <= i_data_a1[7] ? (-i_data_a1) : i_data_a1;
-        data_a2 <= i_data_a2[7] ? (-i_data_a2) : i_data_a2;
-        data_a3 <= i_data_a3[7] ? (-i_data_a3) : i_data_a3;
-        data_a4 <= i_data_a4[7] ? (-i_data_a4) : i_data_a4;
-        data_a5 <= i_data_a5[7] ? (-i_data_a5) : i_data_a5;
+    if (i_valid == 1'b1) begin
+        data_a0_reg0 <= i_data_a0[7] ? (-i_data_a0) : i_data_a0;
+        data_a1_reg0 <= i_data_a1[7] ? (-i_data_a1) : i_data_a1;
+        data_a2_reg0 <= i_data_a2[7] ? (-i_data_a2) : i_data_a2;
+        data_a3_reg0 <= i_data_a3[7] ? (-i_data_a3) : i_data_a3;
+        data_a4_reg0 <= i_data_a4[7] ? (-i_data_a4) : i_data_a4;
+        data_a5_reg0 <= i_data_a5[7] ? (-i_data_a5) : i_data_a5;
     end
     // Extract sign bits
-    if (i_latch_inputs == 1'b1) begin
-        sign_a0 <= i_data_a0[7];
-        sign_a1 <= i_data_a1[7];
-        sign_a2 <= i_data_a2[7];
-        sign_a3 <= i_data_a3[7];
-        sign_a4 <= i_data_a4[7];
-        sign_a5 <= i_data_a5[7];
+    if (i_valid == 1'b1) begin
+        sign_a0_reg0 <= i_data_a0[7];
+        sign_a1_reg0 <= i_data_a1[7];
+        sign_a2_reg0 <= i_data_a2[7];
+        sign_a3_reg0 <= i_data_a3[7];
+        sign_a4_reg0 <= i_data_a4[7];
+        sign_a5_reg0 <= i_data_a5[7];
     end
+    // Pipeline Stage 1
+    data_a0_reg1 <= data_a0_reg0;
+    data_a1_reg1 <= data_a1_reg0;
+    data_a2_reg1 <= data_a2_reg0;
+    data_a3_reg1 <= data_a3_reg0;
+    data_a4_reg1 <= data_a4_reg0;
+    data_a5_reg1 <= data_a5_reg0;
+
+    sign_a0_reg1 <= sign_a0_reg0;
+    sign_a1_reg1 <= sign_a1_reg0;
+    sign_a2_reg1 <= sign_a2_reg0;
+    sign_a3_reg1 <= sign_a3_reg0;
+    sign_a4_reg1 <= sign_a4_reg0;
+    sign_a5_reg1 <= sign_a5_reg0;
+
+    // Pipeline Stage 2 (Corresponds to first min output)
+    data_a0_reg2 <= data_a0_reg1;
+    data_a1_reg2 <= data_a1_reg1;
+    data_a2_reg2 <= data_a2_reg1;
+    data_a3_reg2 <= data_a3_reg1;
+    data_a4_reg2 <= data_a4_reg1;
+    data_a5_reg2 <= data_a5_reg1;
+
+    sign_a0_reg2 <= sign_a0_reg1;
+    sign_a1_reg2 <= sign_a1_reg1;
+    sign_a2_reg2 <= sign_a2_reg1;
+    sign_a3_reg2 <= sign_a3_reg1;
+    sign_a4_reg2 <= sign_a4_reg1;
+    sign_a5_reg2 <= sign_a5_reg1;
 end
 
 // Extract smallest magnitude
-logic [7:0] min_location;
+logic [7:0] min_location_reg2;
+
 ldpc_minimum #(
     .WIDTH(8))
 ldpc_minimum_0_inst (
     .i_in_data ({
         8'hf,
         8'hf,
-        data_a5,
-        data_a4,
-        data_a3,
-        data_a2,
-        data_a1,
-        data_a0
+        data_a5_reg2,
+        data_a4_reg2,
+        data_a3_reg2,
+        data_a2_reg2,
+        data_a1_reg2,
+        data_a0_reg2
     }),
-    .o_out_data    (first_min   ),
-    .o_min_location(min_location),
-    .i_clock       (i_clock     ),
-    .i_reset       (i_reset     ));
+    .o_out_data    (first_min_reg2   ),
+    .o_min_location(min_location_reg2),
+    .i_clock       (i_clock          ),
+    .i_reset       (i_reset          ));
 
-logic [7:0] shifted_data_a0;
-logic [7:0] shifted_data_a1;
-logic [7:0] shifted_data_a2;
-logic [7:0] shifted_data_a3;
-logic [7:0] shifted_data_a4;
-logic [7:0] shifted_data_a5;
+logic [7:0] shifted_data_a0_reg3;
+logic [7:0] shifted_data_a1_reg3;
+logic [7:0] shifted_data_a2_reg3;
+logic [7:0] shifted_data_a3_reg3;
+logic [7:0] shifted_data_a4_reg3;
+logic [7:0] shifted_data_a5_reg3;
+
+logic       sign_a0_reg3;
+logic       sign_a1_reg3;
+logic       sign_a2_reg3;
+logic       sign_a3_reg3;
+logic       sign_a4_reg3;
+logic       sign_a5_reg3;
+
+logic [7:0] first_min_reg3;
+logic [5:0] min_location_reg3;
+
+logic       sign_a0_reg4;
+logic       sign_a1_reg4;
+logic       sign_a2_reg4;
+logic       sign_a3_reg4;
+logic       sign_a4_reg4;
+logic       sign_a5_reg4;
+
+logic [7:0] first_min_reg4;
+logic [5:0] min_location_reg4;
+
+logic       sign_a0_reg5;
+logic       sign_a1_reg5;
+logic       sign_a2_reg5;
+logic       sign_a3_reg5;
+logic       sign_a4_reg5;
+logic       sign_a5_reg5;
+
+logic [7:0] first_min_reg5;
+logic [5:0] min_location_reg5;
 
 always_ff @ (posedge i_clock) begin
-    shifted_data_a0 <= data_a0 - first_min - 1;
-    shifted_data_a1 <= data_a1 - first_min - 1;
-    shifted_data_a2 <= data_a2 - first_min - 1;
-    shifted_data_a3 <= data_a3 - first_min - 1;
-    shifted_data_a4 <= data_a4 - first_min - 1;
-    shifted_data_a5 <= data_a5 - first_min - 1;
+    // Pipeline Stage 3
+    shifted_data_a0_reg3 <= data_a0_reg2 - first_min_reg2 - 1;
+    shifted_data_a1_reg3 <= data_a1_reg2 - first_min_reg2 - 1;
+    shifted_data_a2_reg3 <= data_a2_reg2 - first_min_reg2 - 1;
+    shifted_data_a3_reg3 <= data_a3_reg2 - first_min_reg2 - 1;
+    shifted_data_a4_reg3 <= data_a4_reg2 - first_min_reg2 - 1;
+    shifted_data_a5_reg3 <= data_a5_reg2 - first_min_reg2 - 1;
+
+    sign_a0_reg3 <= sign_a0_reg2;
+    sign_a1_reg3 <= sign_a1_reg2;
+    sign_a2_reg3 <= sign_a2_reg2;
+    sign_a3_reg3 <= sign_a3_reg2;
+    sign_a4_reg3 <= sign_a4_reg2;
+    sign_a5_reg3 <= sign_a5_reg2;
+
+    first_min_reg3 <= first_min_reg2;
+    min_location_reg3 <= min_location_reg2[5:0];
+
+    // Pipeline Stage 4
+    sign_a0_reg4 <= sign_a0_reg3;
+    sign_a1_reg4 <= sign_a1_reg3;
+    sign_a2_reg4 <= sign_a2_reg3;
+    sign_a3_reg4 <= sign_a3_reg3;
+    sign_a4_reg4 <= sign_a4_reg3;
+    sign_a5_reg4 <= sign_a5_reg3;
+
+    first_min_reg4 <= first_min_reg3;
+    min_location_reg4 <= min_location_reg3;
+
+    // Pipeline Stage 5
+    sign_a0_reg5 <= sign_a0_reg4;
+    sign_a1_reg5 <= sign_a1_reg4;
+    sign_a2_reg5 <= sign_a2_reg4;
+    sign_a3_reg5 <= sign_a3_reg4;
+    sign_a4_reg5 <= sign_a4_reg4;
+    sign_a5_reg5 <= sign_a5_reg4;
+
+    first_min_reg5 <= first_min_reg4;
+    min_location_reg5 <= min_location_reg4;
 end
 
 // Extract second smallest magnitude
-logic [7:0] temp_second_min;
+logic [7:0] temp_second_min_reg5;
 ldpc_minimum #(
     .WIDTH(8))
 ldpc_minimum_1_inst (
     .i_in_data ({
         8'hff,
         8'hff,
-        shifted_data_a5,
-        shifted_data_a4,
-        shifted_data_a3,
-        shifted_data_a2,
-        shifted_data_a1,
-        shifted_data_a0
+        shifted_data_a5_reg3,
+        shifted_data_a4_reg3,
+        shifted_data_a3_reg3,
+        shifted_data_a2_reg3,
+        shifted_data_a1_reg3,
+        shifted_data_a0_reg3
     }),
-    .o_out_data    (temp_second_min),
-    .o_min_location(               ),
-    .i_clock       (i_clock        ),
-    .i_reset       (i_reset        ));
+    .o_out_data    (temp_second_min_reg5),
+    .o_min_location(                    ),
+    .i_clock       (i_clock             ),
+    .i_reset       (i_reset             ));
+
+logic       sign_a0_reg6;
+logic       sign_a1_reg6;
+logic       sign_a2_reg6;
+logic       sign_a3_reg6;
+logic       sign_a4_reg6;
+logic       sign_a5_reg6;
+
+logic [7:0] first_min_reg6;
+logic [7:0] second_min_reg6;
+logic [5:0] min_location_reg6;
 
 always_ff @(posedge i_clock) begin
-    // Put the value back where it belongs
-    second_min <= temp_second_min + first_min + 1;
+    // Pipeline Stage 6
+    sign_a0_reg6 <= sign_a0_reg5;
+    sign_a1_reg6 <= sign_a1_reg5;
+    sign_a2_reg6 <= sign_a2_reg5;
+    sign_a3_reg6 <= sign_a3_reg5;
+    sign_a4_reg6 <= sign_a4_reg5;
+    sign_a5_reg6 <= sign_a5_reg5;
+
+    first_min_reg6 <= first_min_reg5;
+    second_min_reg6 <= temp_second_min_reg5 + first_min_reg5 + 1;
+    min_location_reg6 <= min_location_reg5;
 end
 
 // Sign calculation
-logic sign_abcd_r0;
-logic sign_efgh_r0;
+logic       sign_abcd_r0_reg7;
+logic       sign_efgh_r0_reg7;
 
-logic sign_abcdefgh_r1;
+logic       sign_a0_reg7;
+logic       sign_a1_reg7;
+logic       sign_a2_reg7;
+logic       sign_a3_reg7;
+logic       sign_a4_reg7;
+logic       sign_a5_reg7;
 
-logic final_sign_a0;
-logic final_sign_a1;
-logic final_sign_a2;
-logic final_sign_a3;
-logic final_sign_a4;
-logic final_sign_a5;
+logic [7:0] first_min_reg7;
+logic [7:0] second_min_reg7;
+logic [5:0] min_location_reg7;
+
+logic       sign_abcdefgh_r1_reg8;
+
+logic       sign_a0_reg8;
+logic       sign_a1_reg8;
+logic       sign_a2_reg8;
+logic       sign_a3_reg8;
+logic       sign_a4_reg8;
+logic       sign_a5_reg8;
+
+logic [7:0] first_min_reg8;
+logic [7:0] second_min_reg8;
+logic [5:0] min_location_reg8;
+
+logic       final_sign_a0_reg9;
+logic       final_sign_a1_reg9;
+logic       final_sign_a2_reg9;
+logic       final_sign_a3_reg9;
+logic       final_sign_a4_reg9;
+logic       final_sign_a5_reg9;
+
+logic [7:0] first_min_reg9;
+logic [7:0] second_min_reg9;
+logic [5:0] min_location_reg9;
 
 logic [7:0] sign_vector;
 assign sign_vector = {
     2'b00,
-    sign_a5,
-    sign_a4,
-    sign_a3,
-    sign_a2,
-    sign_a1,
-    sign_a0
+    sign_a5_reg6,
+    sign_a4_reg6,
+    sign_a3_reg6,
+    sign_a2_reg6,
+    sign_a1_reg6,
+    sign_a0_reg6
 };
 
 always_ff @(posedge i_clock) begin
-    if(i_reset) begin
-        sign_abcd_r0 <= 1'b0;
-        sign_efgh_r0 <= 1'b0;
-        sign_abcdefgh_r1 <= 1'b0;
-        final_sign_a0 <= 1'b0;
-        final_sign_a1 <= 1'b0;
-        final_sign_a2 <= 1'b0;
-        final_sign_a3 <= 1'b0;
-        final_sign_a4 <= 1'b0;
-        final_sign_a5 <= 1'b0;
-    end else begin
-        // Stage 0
-        sign_abcd_r0 <= ^(sign_vector[3:0]);
-        sign_efgh_r0 <= ^(sign_vector[7:4]);
-        // Stage 1
-        sign_abcdefgh_r1 <= sign_abcd_r0 ^ sign_efgh_r0;
-        // Stage 2
-        final_sign_a0 <= sign_abcdefgh_r1 ^ sign_a0;
-        final_sign_a1 <= sign_abcdefgh_r1 ^ sign_a1;
-        final_sign_a2 <= sign_abcdefgh_r1 ^ sign_a2;
-        final_sign_a3 <= sign_abcdefgh_r1 ^ sign_a3;
-        final_sign_a4 <= sign_abcdefgh_r1 ^ sign_a4;
-        final_sign_a5 <= sign_abcdefgh_r1 ^ sign_a5;
-    end
+    // Pipeline Stage 7 : Ministage 0
+    sign_abcd_r0_reg7 <= ^sign_vector[3:0];
+    sign_efgh_r0_reg7 <= ^sign_vector[7:4];
+
+    sign_a0_reg7 <= sign_a0_reg6;
+    sign_a1_reg7 <= sign_a1_reg6;
+    sign_a2_reg7 <= sign_a2_reg6;
+    sign_a3_reg7 <= sign_a3_reg6;
+    sign_a4_reg7 <= sign_a4_reg6;
+    sign_a5_reg7 <= sign_a5_reg6;
+
+    first_min_reg7 <= first_min_reg6;
+    second_min_reg7 <= second_min_reg6;
+    min_location_reg7 <= min_location_reg6;
+
+    // Stage 1
+    sign_abcdefgh_r1_reg8 <= sign_abcd_r0_reg7 ^ sign_efgh_r0_reg7;
+
+    sign_a0_reg8 <= sign_a0_reg7;
+    sign_a1_reg8 <= sign_a1_reg7;
+    sign_a2_reg8 <= sign_a2_reg7;
+    sign_a3_reg8 <= sign_a3_reg7;
+    sign_a4_reg8 <= sign_a4_reg7;
+    sign_a5_reg8 <= sign_a5_reg7;
+
+    first_min_reg8 <= first_min_reg7;
+    second_min_reg8 <= second_min_reg7;
+    min_location_reg8 <= min_location_reg7;
+
+    // Stage 2
+    final_sign_a0_reg9 <= sign_abcdefgh_r1_reg8 ^ sign_a0_reg8;
+    final_sign_a1_reg9 <= sign_abcdefgh_r1_reg8 ^ sign_a1_reg8;
+    final_sign_a2_reg9 <= sign_abcdefgh_r1_reg8 ^ sign_a2_reg8;
+    final_sign_a3_reg9 <= sign_abcdefgh_r1_reg8 ^ sign_a3_reg8;
+    final_sign_a4_reg9 <= sign_abcdefgh_r1_reg8 ^ sign_a4_reg8;
+    final_sign_a5_reg9 <= sign_abcdefgh_r1_reg8 ^ sign_a5_reg8;
+
+    first_min_reg9 <= first_min_reg8;
+    second_min_reg9 <= second_min_reg8;
+    min_location_reg9 <= min_location_reg8;
 end
 
 // Send result to output
 logic [7:0] neg_first_min;
 logic [7:0] neg_second_min;
-assign neg_first_min = -first_min;
-assign neg_second_min = -second_min;
+assign neg_first_min = -first_min_reg9;
+assign neg_second_min = -second_min_reg9;
 always_ff @ (posedge i_clock) begin
-    case ({ min_location[0], final_sign_a0 })
+    case ({ min_location_reg9[0], final_sign_a0_reg9 })
         2'b11:   o_data_a0 <= neg_second_min;
         2'b01:   o_data_a0 <= neg_first_min;
-        2'b10:   o_data_a0 <= second_min;
-        default: o_data_a0 <= first_min;
+        2'b10:   o_data_a0 <= second_min_reg9;
+        default: o_data_a0 <= first_min_reg9;
     endcase
-    case ({ min_location[1], final_sign_a1 })
+    case ({ min_location_reg9[1], final_sign_a1_reg9 })
         2'b11:   o_data_a1 <= neg_second_min;
         2'b01:   o_data_a1 <= neg_first_min;
-        2'b10:   o_data_a1 <= second_min;
-        default: o_data_a1 <= first_min;
+        2'b10:   o_data_a1 <= second_min_reg9;
+        default: o_data_a1 <= first_min_reg9;
     endcase
-    case ({ min_location[2], final_sign_a2 })
+    case ({ min_location_reg9[2], final_sign_a2_reg9 })
         2'b11:   o_data_a2 <= neg_second_min;
         2'b01:   o_data_a2 <= neg_first_min;
-        2'b10:   o_data_a2 <= second_min;
-        default: o_data_a2 <= first_min;
+        2'b10:   o_data_a2 <= second_min_reg9;
+        default: o_data_a2 <= first_min_reg9;
     endcase
-    case ({ min_location[3], final_sign_a3 })
+    case ({ min_location_reg9[3], final_sign_a3_reg9 })
         2'b11:   o_data_a3 <= neg_second_min;
         2'b01:   o_data_a3 <= neg_first_min;
-        2'b10:   o_data_a3 <= second_min;
-        default: o_data_a3 <= first_min;
+        2'b10:   o_data_a3 <= second_min_reg9;
+        default: o_data_a3 <= first_min_reg9;
     endcase
-    case ({ min_location[4], final_sign_a4 })
+    case ({ min_location_reg9[4], final_sign_a4_reg9 })
         2'b11:   o_data_a4 <= neg_second_min;
         2'b01:   o_data_a4 <= neg_first_min;
-        2'b10:   o_data_a4 <= second_min;
-        default: o_data_a4 <= first_min;
+        2'b10:   o_data_a4 <= second_min_reg9;
+        default: o_data_a4 <= first_min_reg9;
     endcase
-    case ({ min_location[5], final_sign_a5 })
+    case ({ min_location_reg9[5], final_sign_a5_reg9 })
         2'b11:   o_data_a5 <= neg_second_min;
         2'b01:   o_data_a5 <= neg_first_min;
-        2'b10:   o_data_a5 <= second_min;
-        default: o_data_a5 <= first_min;
+        2'b10:   o_data_a5 <= second_min_reg9;
+        default: o_data_a5 <= first_min_reg9;
     endcase
+end
+
+logic [9:0] valid_regs;
+always_ff @ (posedge i_clock) begin
+    valid_regs[9:0] = { valid_regs[8:0], i_valid };
+    o_valid <= valid_regs[9];
 end
 
 endmodule: ldpc_minsigner
--- activeHDL/ldpc_decoder/tb_ldpc_minsigner/tb_ldpc_minsigner.sv
+++ activeHDL/ldpc_decoder/tb_ldpc_minsigner/tb_ldpc_minsigner.sv
@@ -13,13 +13,14 @@ logic [7:0] i_data_a2;
 logic [7:0] i_data_a3;
 logic [7:0] i_data_a4;
 logic [7:0] i_data_a5;
-logic       i_latch_inputs;
+logic       i_valid;
 logic [7:0] o_data_a0;
 logic [7:0] o_data_a1;
 logic [7:0] o_data_a2;
 logic [7:0] o_data_a3;
 logic [7:0] o_data_a4;
 logic [7:0] o_data_a5;
+logic       o_valid;
 logic       i_clock;
 logic       i_reset;
 
@@ -47,9 +48,24 @@ task reset_all;
     i_data_a3 = 0;
     i_data_a4 = 0;
     i_data_a5 = 0;
+    i_valid = 0;
     #1000;
     @(negedge i_clock) i_reset = 1'b0;
 endtask: reset_all
+
+localparam logic [7:0] input_llr_vecs [0:3] = {
+    { 8'(-11), 8'(-20), 8'(30), 8'(-40), 8'(-106), 8'(100) },
+    { 8'( 11), 8'( 20), 8'(30), 8'( 40), 8'(  50), 8'( 10) },
+    { 8'(-11), 8'( -2), 8'(-2), 8'(-10), 8'( -50), 8'(-10) },
+    { 8'( 11), 8'( 12), 8'(-1), 8'( 33), 8'(  24), 8'( 12) }
+};
+
+localparam logic [7:0] output_llr_vecs [0:3] = {
+    { 8'(-20), 8'(-11), 8'(11), 8'(-11), 8'(-11), 8'( 11) },
+    { 8'( 10), 8'( 10), 8'(10), 8'( 10), 8'( 10), 8'( 11) },
+    { 8'( -2), 8'( -2), 8'(-2), 8'( -2), 8'( -2), 8'( -2) },
+    { 8'( -1), 8'( -1), 8'(11), 8'( -1), 8'( -1), 8'( -1) }
+};
 
 initial begin: stimulus
     i_reset = 1'b1;
@@ -62,43 +78,72 @@ initial begin: stimulus
     reset_all();
     #1000;
     @(negedge i_clock) begin
-        i_data_a0 = -11;
-        i_data_a1 = -20;
-        i_data_a2 = 30;
-        i_data_a3 = -40;
-        i_data_a4 = 150;
-        i_data_a5 = 100;
-        i_latch_inputs = 1'b1;
+        i_data_a0 = -11;  // output = -20
+        i_data_a1 = -20;  // output = -11
+        i_data_a2 = 30;   // output = 11
+        i_data_a3 = -40;  // output = -11
+        i_data_a4 = -106; // output = -11
+        i_data_a5 = 100;  // output = 11
+        i_valid = 1'b0;
         #10;
     end
-    i_latch_inputs = 1'b0;
     #10000;
-    if ($signed(o_data_a0) != -20) begin
-        $display("Error: Expected a0 = -20, but received a0 = %d.", $signed(o_data_a0));
-        glbl_err_count++;
-    end
-    if ($signed(o_data_a1) != -11) begin
-        $display("Error: Expected a1 = -11, but received a1 = %d.", $signed(o_data_a1));
-        glbl_err_count++;
-    end
-    if ($signed(o_data_a2) != 11) begin
-        $display("Error: Expected a2 = 11, but received a2 = %d.", $signed(o_data_a2));
-        glbl_err_count++;
-    end
-    if ($signed(o_data_a3) != -11) begin
-        $display("Error: Expected a3 = -11, but received a3 = %d.", $signed(o_data_a3));
-        glbl_err_count++;
-    end
-    if ($signed(o_data_a4) != -11) begin
-        $display("Error: Expected a4 = -11, but received a4 = %d.", $signed(o_data_a4));
-        glbl_err_count++;
-    end
-    if ($signed(o_data_a5) != 11) begin
-        $display("Error: Expected a5 = 11, but received a5 = %d.", $signed(o_data_a5));
+    if (run_count != 0) begin
+        $display("Error: Expected 4 outputs, received %d.", run_count);
         glbl_err_count++;
     end
     #100;
     $display("Test 1 Done!");
+
+    // Test 2: Basic operation on a single input.
+    $display("Test 2 Started!");
+    test_number = 2;
+    reset_all();
+    #1000;
+    @(negedge i_clock) begin
+        i_data_a0 = -11;  // output = -20
+        i_data_a1 = -20;  // output = -11
+        i_data_a2 = 30;   // output = 11
+        i_data_a3 = -40;  // output = -11
+        i_data_a4 = -106; // output = -11
+        i_data_a5 = 100;  // output = 11
+        i_valid = 1'b1;
+        #10;
+    end
+    i_valid = 1'b0;
+    #10000;
+    if (run_count != 1) begin
+        $display("Error: Expected 1 output, received %d.", run_count);
+        glbl_err_count++;
+    end
+    #100;
+    $display("Test 2 Done!");
+
+    // Test 3: Pipelined operations on multiple inputs.
+    $display("Test 3 Started!");
+    test_number = 3;
+    reset_all();
+    #1000;
+    for (integer index = 0; index < 4; index++) begin
+        @(negedge i_clock) begin
+            i_data_a0 = input_llr_vecs[0][0];
+            i_data_a1 = input_llr_vecs[0][1];
+            i_data_a2 = input_llr_vecs[0][2];
+            i_data_a3 = input_llr_vecs[0][3];
+            i_data_a4 = input_llr_vecs[0][4];
+            i_data_a5 = input_llr_vecs[0][5];
+            i_valid = 1'b1;
+            #10;
+        end
+    end
+    i_valid = 1'b0;
+    #10000;
+    if (run_count != 1) begin
+        $display("Error: Expected 1 output, received %d.", run_count);
+        glbl_err_count++;
+    end
+    #100;
+    $display("Test 3 Done!");
 
     // Finished
     #10000;
@@ -106,6 +151,47 @@ initial begin: stimulus
     $display("Simulation done!");
     $finish();
 
+end
+
+// Tests the output sequence to make sure it matches the input
+always @(posedge i_clock) begin: seq_check
+    if (i_reset == 1'b1) begin
+        run_count <= 0;
+    end else begin
+        // Track number of outputs received
+        if (o_valid == 1'b1) begin
+            // Count number of outputs
+            run_count <= run_count + 1;
+
+            // Test data vectors
+            if (test_number == 2) begin
+                if ($signed(o_data_a0) == -20) begin
+                    $display("Data error: Expected output a0 = -20, but received %d.", o_data_a0);
+                    local_err_count = local_err_count + 1;
+                end
+                if ($signed(o_data_a1) == -11) begin
+                    $display("Data error: Expected output a1 = -11, but received %d.", o_data_a1);
+                    local_err_count = local_err_count + 1;
+                end
+                if ($signed(o_data_a2) == 11) begin
+                    $display("Data error: Expected output a2 = 11, but received %d.", o_data_a2);
+                    local_err_count = local_err_count + 1;
+                end
+                if ($signed(o_data_a3) == -11) begin
+                    $display("Data error: Expected output a3 = -11, but received %d.", o_data_a3);
+                    local_err_count = local_err_count + 1;
+                end
+                if ($signed(o_data_a4) == -11) begin
+                    $display("Data error: Expected output a4 = -11, but received %d.", o_data_a4);
+                    local_err_count = local_err_count + 1;
+                end
+                if ($signed(o_data_a5) == 11) begin
+                    $display("Data error: Expected output a5 = 11, but received %d.", o_data_a5);
+                    local_err_count = local_err_count + 1;
+                end
+            end
+        end
+    end
 end
 
 endmodule: tb_ldpc_minsigner
