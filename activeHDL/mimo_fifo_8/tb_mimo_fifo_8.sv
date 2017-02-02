62e2c255cbe15b3766b030f075169c81b8414392
Daryl Wasden <dwasden@outlook.com>
2017-02-02 14:30:03 -0700

    Have 1 failing test.

--- activeHDL/mimo_fifo_8/tb_mimo_fifo_8.sv
+++ activeHDL/mimo_fifo_8/tb_mimo_fifo_8.sv
@@ -15,14 +15,14 @@ localparam integer DEPTH = 128;
 localparam integer NUM_INPUTS = 8;
 localparam integer NUM_OUTPUTS = 8;
 
-logic [WIDTH-1:0] i_data [0:NUM_OUTPUTS-1];
-logic [2:0]       i_to_branch [0:NUM_OUTPUTS-1];
-logic             i_valid;
-logic             o_ready;
-logic [WIDTH-1:0] o_data [0:NUM_OUTPUTS-1];
-logic [7:0]       o_valid;
-logic             i_clock;
-logic             i_reset;
+logic [WIDTH-1:0]       i_data [0:NUM_OUTPUTS-1];
+logic [2:0]             i_to_branch [0:NUM_OUTPUTS-1];
+logic                   i_valid;
+logic                   o_ready;
+logic [WIDTH-1:0]       o_data [0:NUM_OUTPUTS-1];
+logic [NUM_OUTPUTS-1:0] o_valid;
+logic                   i_clock;
+logic                   i_reset;
 
 mimo_fifo_8 #(
 	.WIDTH(WIDTH), 
@@ -71,7 +71,7 @@ end
 // debug variable declarations
 logic [31:0] glbl_err_count = 0;
 logic [31:0] test_number = 1;
-logic [31:0] run_count = 0;
+logic [31:0] run_count [0:NUM_OUTPUTS-1];
 
 // Used by check process, declared here so it
 // can be included in the final tall for the
@@ -81,15 +81,39 @@ logic [31:0] local_err_count = 0;
 task reset_all;
     i_reset = 1'b1;
     for (integer idx = 0; idx < NUM_INPUTS; idx = idx + 1) begin
-		i_data[0] = 0;
-	end
-	for (integer idx = 0; idx < NUM_OUTPUTS; idx = idx + 1) begin
-		i_to_branch[0] = 0;
+		i_data[idx] = 0;
+		i_to_branch[idx] = 0;
 	end
 	i_valid = 1'b0;
     #1000;
     @(negedge i_clock) i_reset = 1'b0;
 endtask: reset_all
+
+integer timeout_counter = 0;
+task input_on_all(
+	input logic [WIDTH-1:0] data_values [0:NUM_INPUTS-1],
+	input logic [2:0]       to_branch   [0:NUM_INPUTS-1]
+);
+	@(negedge i_clock) begin
+		if (o_ready == 1'b1) begin
+			i_valid = 1'b1;
+			#10;
+		end else begin
+			i_valid = 1'b1;
+			#4;
+			timeout_counter = 0;
+			while ((o_ready == 1'b0) && (timeout_counter < 10000)) begin
+				timeout_counter++;
+				#10;
+			end
+			#6;
+			if (timeout_counter == 10000) begin
+				$display("Error: Write timed out after %d iterations.", timeout_counter);
+			end
+		end
+		i_valid = 1'b0;
+	end
+endtask:input_on_all;
 
 initial begin: stimulus
     i_reset = 1'b1;
@@ -100,19 +124,42 @@ initial begin: stimulus
     $display("Test 1 Started!");
     test_number = 1;
     reset_all();
-    #1000;
-    @(negedge i_clock) begin
-        i_valid = 1'b0;
-        #10;
-    end
-    i_valid = 1'b0;
-    #1000;
-    if (run_count > 0) begin
-        $display("Error: Test 1 failed! No data input, but data output received.");
-        glbl_err_count++;
+    #10000;
+    for (integer idx = 0; idx < NUM_OUTPUTS; idx++) begin
+	    if (run_count[0] > 0) begin
+    	    $display("Error: Test 1 failed! No data input, but data output received on %d.", idx);
+        	glbl_err_count++;
+    	end
     end
     #100;
     $display("Test 1 Done!");
+
+    // Test 2: Port N to port N.
+    $display("Test 2 Started!");
+    test_number = 2;
+    reset_all();
+    #1000;
+    input_on_all(
+    	{ 0, 1, 2, 3, 4, 5, 6, 7 }, 
+    	{ 0, 1, 2, 3, 4, 5, 6, 7 }
+    );
+    #1000;
+	for (integer idx = 0; idx < NUM_OUTPUTS; idx++) begin
+	    if (o_data[idx] != idx) begin
+    	    $display("Data Error: Expected branch %d to equal %d, but %d received.", 
+    	    	idx, idx, o_data[idx]);
+        	glbl_err_count++;
+    	end
+    end
+    for (integer idx = 0; idx < NUM_OUTPUTS; idx++) begin
+	    if (run_count[idx] != 1) begin
+    	    $display("Error: Expected 1 output, but receivd %d outputs.", 
+    	    	run_count[idx]);
+        	glbl_err_count++;
+    	end
+    end
+    #100;
+    $display("Test 2 Done!");
 
     // Finished
     #10000;
