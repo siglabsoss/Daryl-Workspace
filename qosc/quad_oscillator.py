928619fef26ba94af5c73c8c89df1b84fcd7652d
Daryl Wasden <dwasden@outlook.com>
2017-02-10 09:07:36 -0700

    A simple recursive quadrature oscillator.

---
+++ qosc/quad_oscillator.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as pt
+
+N = 39 * 250
+fc = 39.0 / 250.0
+k1 = 2**15 * np.tan(np.pi*fc)
+k2 = np.sin(2*np.pi*fc)
+
+u, v = 1.0, 0.0
+k1v = 0.0
+
+x = np.zeros(N, dtype=np.complex)
+
+for index in range(N):
+    x[index] = u + 1j * v
+    w = u - k1v
+    v = v + k2 * w
+    k1v = k1 * v
+    u = w - k1v
+
+pt.figure()
+pt.plot(np.real(x))
+pt.plot(np.imag(x))
+
+pt.figure()
+freqs = np.arange(-N/2, N/2) / float(N)
+X = np.fft.fftshift(np.fft.fft(x, N))
+pt.plot(freqs, 20*np.log10(np.abs(X)))
+
+pt.show()
