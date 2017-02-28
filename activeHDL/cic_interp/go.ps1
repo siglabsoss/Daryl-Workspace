32eab07a8d5da0c43f23211d5444aca2094bc1d8
Daryl Wasden <dwasden@outlook.com>
2017-02-28 14:57:27 -0800

    First commit of the CIC interpolator.

--- activeHDL/cic_interp/go.ps1
+++ activeHDL/cic_interp/go.ps1
@@ -18,14 +18,8 @@ function Compile-Verilog
 vlib work
 
 # Compile the Verilog Files
-Compile-Verilog tb_fixed_ddsx2.sv
-Compile-Verilog fixed_ddsx2.sv
-
-# Clean up the output before processing next run
-Remove-Item outvec.txt -ErrorAction SilentlyContinue
+Compile-Verilog tb_cic_interp.sv
+Compile-Verilog cic_interp.sv
 
 # Execute the Simulation
 vsimsa -do dosim.do
-
-# Run Python data analysis on output
-python .\python\test_data.py
