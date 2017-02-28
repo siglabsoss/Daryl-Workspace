572ef8a8eac16841ecdf73087c830a6ac33b8588
Daryl Wasden <dwasden@outlook.com>
2017-02-28 14:56:22 -0800

    Added a few lines to remove extraneous files from previous runs.

--- activeHDL/fixed_ddsx2/go.ps1
+++ activeHDL/fixed_ddsx2/go.ps1
@@ -21,5 +21,11 @@ vlib work
 Compile-Verilog tb_fixed_ddsx2.sv
 Compile-Verilog fixed_ddsx2.sv
 
+# Clean up the output before processing next run
+Remove-Item outvec.txt -ErrorAction SilentlyContinue
+
 # Execute the Simulation
 vsimsa -do dosim.do
+
+# Run Python data analysis on output
+python .\python\test_data.py
