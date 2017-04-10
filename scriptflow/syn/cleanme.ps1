2aa78f5db6c54b3756a807e8d62a7435f78e2f19
Daryl Wasden <dwasden@outlook.com>
2017-04-10 13:57:29 -0700

    Removed some files, added a cleanme script.

---
+++ scriptflow/syn/cleanme.ps1
@@ -0,0 +1,4 @@
+Remove-Item ./rev_* -recurse -ErrorAction SilentlyContinue
+Remove-Item ./stdout.log* -ErrorAction SilentlyContinue
+Remove-Item ./synlog.tcl -ErrorAction SilentlyContinue
+Remove-Item ./test.prj -ErrorAction SilentlyContinue
--- scriptflow/syn/synlog.tcl
+++
@@ -1,4 +0,0 @@
-source "C:/Users/user/AppData/Local/Synplicity/scm_perforce.tcl"
-history clear
-project -load C:/Users/user/mygit/Daryl-Workspace/scriptflow/syn/test.prj
-project -close C:/Users/user/mygit/Daryl-Workspace/scriptflow/syn/test.prj
