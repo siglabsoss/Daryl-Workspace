d49eca4e136370bca7056f707aa04cc42246ca84
Daryl <daryl@siglabs.com>
2017-08-02 10:27:08 -0600

    Modifications to DDS.

--- cpp/graviton_interface/sysdef.h
+++ cpp/graviton_interface/sysdef.h
@@ -7,13 +7,19 @@ namespace sysdef {
     ////////////////////////////////////////////////
     // LINK CONSTANTS
     ////////////////////////////////////////////////
-    constexpr char DAC_DATA_IP[]     = "10.0.0.10"; //"192.168.2.3";
-    constexpr char DAC_DATA_PORT[]   = "60000";
+    constexpr char DAC_DATA_IP[]     = "192.168.2.2";
+    constexpr char DAC_DATA_PORT[]   = "50000";
     constexpr int  DAC_PACKET_LENGTH = 1472-4;
 
-    constexpr char ADC_DATA_PORT[]   = "50000";
+    constexpr char ADC_DATA_PORT[]   = "50001";
     constexpr char ADC_DATA_TIMEOUT  = 100; // microseconds
     constexpr int  ADC_PACKET_LENGTH = 1472;
+
+    constexpr char CCTX_DATA_IP[]     = "192.168.2.2";
+    constexpr char CCTX_DATA_PORT[]   = "60000";
+
+    constexpr char ADC_DATA_PORT[]   = "60001";
+    constexpr char ADC_DATA_TIMEOUT  = 100; // microseconds
 
     ////////////////////////////////////////////////
     // MAGIC NUMBER CONSTANTS
@@ -40,6 +46,11 @@ namespace sysdef {
     // DSP NORMALIZATION CONSTANTS
     ////////////////////////////////////////////////
     constexpr double DAC_FULL_SCALE = 27969.0;
+
+    ////////////////////////////////////////////////
+    // COMMAND AND CONTROL INTERFACE
+    ////////////////////////////////////////////////
+    constexpr
 
 } // end sysdef
 
